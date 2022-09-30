4c2cfdb4635cfb9876519da97950889c68a3fd04
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-29 21:57:35 -0300

    o básico de var, let e const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,22 @@
+/*
+VAR | LET | CONST
+*/
+
+var a = 3
+let b = 4
+
+//Reatribuindo um novo valor
+var a = 30
+b = 40
+
+/*
+Podemos notar que o var permite repetir a atribuição de uma variável repetida no mesmo código. De modo diferente, o let não permite!
+*/
+
+console.log(a,b);
+
+const nome = 'Ayrton'
+//nome = 'Barreto'
+console.log(nome);
+
+//Constantes não permitem a atribuição de um novo valor
