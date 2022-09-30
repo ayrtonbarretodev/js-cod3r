ab7c9a69713879e4c3470bcac6ea7ffabf77148c
Ayrton <devayrtonsbarreto@gmail.com>
2022-09-29 22:03:59 -0300

    tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+/*
+A tipagem fraca está ligada a característica da linguagem de realizar conversões automaticamente entre tipos diferentes de dados.
+
+JAVA - Tipagem Forte
+JavaScript - Tipagem Fraca
+*/
+
+let nome = "Ayrton Barreto"; //String
+console.log(typeof(nome))
+
+let idade = 28; //Number
+console.log(typeof(idade))
+
