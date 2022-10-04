cc699037d9e6a5f58cd9921fe62389f3d847ccb4
Ayrton <devayrtonsbarreto@gmail.com>
2022-10-03 22:15:04 -0300

    47 - Object

---
+++ fundamentos/object.js
@@ -0,0 +1,23 @@
+//O objeto em JavaScript é uma coleção de chave : valor
+
+//Forma 1
+const prod1 ={}
+prod1.nome = "Celular Ultra Mega"
+prod1.preco = 4998.90
+console.log(prod1)
+
+
+//Forma 2
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    info: {
+        id:1,
+        categoria:{
+            nome: 'Roupa'
+        }
+    }
+}
+
+console.log(prod2)
+
