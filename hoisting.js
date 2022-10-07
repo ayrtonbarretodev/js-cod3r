e89fc5c2ea68bdbc77db24beae0da862197850ad
Ayrton <devayrtonsbarreto@gmail.com>
2022-10-07 19:30:05 -0300

    59 - Entendendo o Hoisting

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+
+//Hoisting é um comportamento padrão de mover a declaração para o topo
+
+console.log('a =', a)
+var a = 2;
+console.log('a =', a)
+
+console.log('b =', b)
+let b = 2
+console.log('b =', b)
